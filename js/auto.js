c2e8700390cd61dd62c475fd0c56ea849f8b9cfa
LisovaOl <lisova.olenka@gmail.com>
2022-06-10 09:10:28 +0300

    xc

--- js/auto.js
+++ js/auto.js
@@ -1,19 +1,29 @@
-function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-    let message;
-    pricePerDroid = 100;
-    orderedQuantity = 5;
-    customerCredits = 1000;
-    // Change code below this line
-    let totalPrice = pricePerDroid * orderedQuantity;
-    console.log(totalPrice);
-}
-// if(totalPrice > customerCredits){
-//   message = "Insufficient funds!";
-// }
-// else(customerCredits - totalPrice){
-// message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`;
+// Функция getShippingCost(country) должна проверять возможность доставки товара в страну пользователя (параметр country) и возвращать сообщение о результате хранящееся в переменной message. Обязательно используй инструкцию switch.
 
-// }
-//   // Change code above this line
-//   return message;
-// }
+// Формат возвращаемой строки "Shipping to <country> will cost <price> credits", где вместо <country> и <price> необходимо подставить соотвествующие значения.
+
+// Список стран и стоимость доставки:
+
+//     China - 100 кредитов
+//     Chile - 250 кредитов
+//     Australia - 170 кредитов
+//     Jamaica - 120 кредитов
+
+// Из списка видно, что доставка есть не везде. Если указанной страны нет в списке, то функция должна вернуть строку "Sorry, there is no delivery to your country"
+
+function getShippingCost(country) {
+  let message;
+  // Change code below this line
+
+  let country = 'China', 'Chile', 'Australia', 'Jamaica';
+
+  switch (getShippingCost(country)) {
+    case 'China':
+      price = 100;
+      message = 'Shipping to ${country} will cost ${price} credits';
+      break;
+  }
+  // Change code above this line
+  return message;
+}
+getShippingCost(country);
--- js/switch.js
+++ js/switch.js
@@ -1,23 +1,123 @@
 
 // Оператор break в завершении каждого блока case необходим чтобы прервать дальнейшие проверки и сразу перейти к коду за switch в том случае, когда проверка на равенство вернула true
-let cost;
-const subscription = "premium";
+// let cost;
+// const subscription = "premium";
 
-switch (subscription) {
-  case "free":
-    cost = 0;
+// switch (subscription) {
+//   case "free":
+//     cost = 0;
+//     break;
+
+//   case "pro":
+//     cost = 100;
+//     break;
+
+//   case "premium":
+//     cost = 500;
+//     break;
+
+//   default:
+//     console.log("Invalid subscription type");
+// }
+
+// console.log(cost); // 500
+
+
+// const stars = 8;
+// let price;
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 30;
+//         break;
+//     case 3:
+//         price = 40;
+//         break;
+//     case 4:
+//         price = 50;
+//         break;
+//     case 5:
+//         price = 60;
+//         break;
+//     default:
+//         console.log('Такое количество не найдено.');
+// }
+// console.log(price);
+
+/*
+ * Напиши скрипт выбора стоимости отеля по кол-ву звёзд.
+ * 1,2 - 20$, 3,4 - 30$, 5 - 120$
+ */
+
+// const stars = 1;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//   price = 20;
+// } else if (stars === 3 || stars === 4) {
+//   price = 30;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Такого кол-ва звезд нет");
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log("Такого кол-ва звезд нет");
+// }
+/*
+ * Напиши скрипт выбора опции доставки товара.
+ * Опция хранится в переменной option: 1 - самовывоз, 2 - курьер, 3 - почта
+ *
+ * В переменную message записать сообщение в зависимости от опции.
+ * - 'Вы сможете забрать товар завтра с 12:00 в нашем офисе'
+ * - 'Курьер доставит заказ завтра с 9:00 до 18:00'
+ * - 'Посылка будет отправлена сегодня'
+ * - 'Вам перезвонит менеджер'
+ */
+
+//  1. сделать переменные
+const option = 1;
+let message = "";
+
+// 2. сделать switch 1 2 3
+// 3. в каждом кейсе записать в message строку
+switch (option) {
+  case 1:
+    message = "Вы сможете забрать товар завтра с 12:00 в нашем офисе";
     break;
 
-  case "pro":
-    cost = 100;
+  case 2:
+    message = "Курьер доставит заказ завтра с 9:00 до 18:00";
     break;
 
-  case "premium":
-    cost = 500;
+  case 3:
+    message = "Посылка будет отправлена сегодня";
     break;
 
   default:
-    console.log("Invalid subscription type");
+    message = "Вам перезвонит менеджер";
 }
 
-console.log(cost); // 500
+// 4. сделать лог message
+console.log(message);
