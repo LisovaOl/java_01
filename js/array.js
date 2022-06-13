88bf77bb2c3b4d59eed5ea42e489ff4eafcb92b3
LisovaOl <lisova.olenka@gmail.com>
2022-06-13 06:20:57 +0300

    task

--- js/array.js
+++ js/array.js
@@ -27,7 +27,7 @@
 
 //===============================================
 
-//Передача по ссылке (копия) cложный тип тип 
+//Передача по ссылке (копия) cложный тип тип
 
 // let a = [0, 1, 2, 3];
 
@@ -40,4 +40,81 @@
 // console.log(a);
 // console.log(b);
 // console.log(a === b) //они одинаковые
+
+//================================================
+
+//====== Пепребор массива =================
+
+// const friends = ['andrey', 'paul', 'nazar', 'alex', 'olena', 'yana', 'kira', 'nika'];
+// const lastIndex = friends.length - 1;
+// for (let i = 0; i <= lastIndex; i += 1) {
+//   console.log(friends[i]);
+// }
+
+
+//=========== 2 variant ======
+
+// const lastIndex = friends.length - 1;
+// for (let i = 0; i < friends.length; i += 1) {
+//     friends[i] += '-1'; //add "-1" for each element
+// }
+
+// console.log(friends);
+// console.table(friends);
+
+
+            //     Декларативный цикл. Когда не нужно ничего менять в массиве
+            
+// for (const friend of friends) {
+//     console.log(friend); // display a list of friends one by one . 
+// }
+
+
+// +++++++++++++  Tasks ++++++++++++++
+
+// = = = = = = Task 1 = = = = = = 
+
+// Посчитать общую сумму покупок в корзине
+
+const cart = [54, 28, 105, 70, 92, 17, 120];
+
+    // 2. create a variable 'total' with 0 BEFORE 'for'
+let total = 0;
+
+    // 1. Перебрать елементы цикла
+// for (let i = 0; i < cart.length; i += 1){
+//     //каждый елемент приплюсовать к total
+//     total += cart[i];
+// }
+
+    // 3 .Display
+// console.log('Total:', total)
+
+//======== Кращий варіант ==========    for...of
+
+for (const value of cart) {
+    total += value;
+}
+console.log('Total:', total);
+
+
+// ==== додати до кожного елемента 10 %
+for (let i = 0; i < cart.length; i += 1){
+//     //каждый елемент увеличить на 10 % и округлить
+    cart[i] = Math.round(1.1 * cart[i]);
+    
+}
+console.log('Цена выросла на 10%', cart);
+
+
+
+
+
+
+
+
+
+
+
+
 
